4ac07e5c7818cdd556b81e6c6be81ecb227e83f4
Abdul Rahman Zahiri <abdulrahmanzahiri6644@gmail.com>
2025-06-23 01:51:08 -0230

    Implement custom generic SingleLinkedList with iterable support

--- src/main/java/org/example/linkedlist/SingleLinkedList.java
+++ src/main/java/org/example/linkedlist/SingleLinkedList.java
@@ -1,4 +1,47 @@
 package org.example.linkedlist;
 
-public class SingleLinkedList {
+import java.util.Iterator;
+
+public class SingleLinkedList<T> implements Iterable<T> {
+    private Node<T> head;
+
+    public void add(T value) {
+        Node<T> newNode = new Node<>(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.getNext() != null) {
+                current = current.getNext();
+            }
+            current.setNext(newNode);
+        }
+    }
+
+    public Node<T> getHead() {
+        return head;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private Node<T> current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public T next() {
+                T val = current.getValue();
+                current = current.getNext();
+                return val;
+            }
+        };
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
 }
