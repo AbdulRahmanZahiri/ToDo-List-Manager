1497d6342fdca1b8625d411032e45d500b974c91
Abdul Rahman Zahiri <abdulrahmanzahiri6644@gmail.com>
2025-06-23 01:51:37 -0230

    Define User class with task assignment and completion logic

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,4 +1,27 @@
 package org.example;
 
 public class User {
+    private final String name;
+    private final TaskList taskList;
+
+    public User(String name) {
+        this.name = name;
+        this.taskList = new TaskList();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addTask(Task task) {
+        taskList.addTask(task);
+    }
+
+    public TaskList getTaskList() {
+        return taskList;
+    }
+
+    public boolean markTaskCompleted(String desc) {
+        return taskList.markTaskCompleted(desc);
+    }
 }
