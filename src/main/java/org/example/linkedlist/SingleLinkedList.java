89e7377c5124e0860577fa627afa72da89b29c05
Abdul Rahman Zahiri <abdulrahmanzahiri6644@gmail.com>
2025-06-23 23:10:53 -0230

    added comments to SingleLinkedList.java to explain linked list operations

--- src/main/java/org/example/linkedlist/SingleLinkedList.java
+++ src/main/java/org/example/linkedlist/SingleLinkedList.java
@@ -2,14 +2,27 @@ package org.example.linkedlist;
 
 import java.util.Iterator;
 
+/**
+ * A simple implementation of a generic singly linked list.
+ * Supports adding elements and iteration.
+ *
+ * @param <T> the type of elements stored in the list
+ */
 public class SingleLinkedList<T> implements Iterable<T> {
-    private Node<T> head;
+    private Node<T> head; // The head (first node) of the list
 
+    /**
+     * Adds a new element to the end of the linked list.
+     *
+     * @param value the value to be added
+     */
     public void add(T value) {
         Node<T> newNode = new Node<>(value);
         if (head == null) {
+            // If the list is empty, set new node as head
             head = newNode;
         } else {
+            // Traverse to the last node and add the new node
             Node<T> current = head;
             while (current.getNext() != null) {
                 current = current.getNext();
@@ -18,10 +31,29 @@ public class SingleLinkedList<T> implements Iterable<T> {
         }
     }
 
+    /**
+     * Returns the head node of the list.
+     *
+     * @return head node
+     */
     public Node<T> getHead() {
         return head;
     }
 
+    /**
+     * Returns true if the list is empty.
+     *
+     * @return true if no elements, false otherwise
+     */
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    /**
+     * Allows the list to be used in a for-each loop.
+     *
+     * @return an iterator over the list elements
+     */
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
@@ -39,9 +71,5 @@ public class SingleLinkedList<T> implements Iterable<T> {
                 return val;
             }
         };
-    }
-
-    public boolean isEmpty() {
-        return head == null;
     }
 }
